b16d5feb04608695da9c86222e8f4af6d98f5049
Aili Gong <aili.hgong@gmail.com>
2020-03-13 02:53:42 +0000

    Added bubbleSort2

---
+++ BubbleSort2.java
@@ -0,0 +1,33 @@
+public class BubbleSort2 {
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int n = arr.length;
+        boolean swapped = false;
+
+        for (int i = 0; i < n - 1; i ++) {
+            for (int j = 0; j < n - i - 1; j ++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {9, 8, 3, 1, 5};
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
--- QuickSort2.java
+++ QuickSort2.java
@@ -9,9 +9,9 @@ public class QuickSort2 {
         }
     }
 
-    public static int partition(int[] arr, int low, int high) {
+    private static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-        
+
         int i = low - 1;
         for (int j = low; j < high; j++) {
             if (arr[j] < pivot) {

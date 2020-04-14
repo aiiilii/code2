65af4e81caffeeaef059309a57091a43b8b585d9
Aili Gong <aili.hgong@gmail.com>
2020-04-14 00:48:56 +0000

    Added QuickSort4

---
+++ QuickSort4.java
@@ -0,0 +1,38 @@
+public class QuickSort4 {
+
+    public static void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i ++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {9, 8, 3, 1, 5, 4, 6, 8, 7, 7, 4, 1, 2};
+        sort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
